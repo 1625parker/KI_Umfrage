175790bd8036ef8c4c0c346aaaeefa7ad82ff4a8
1625parker <102374733+1625parker@users.noreply.github.com>
2026-09-22 13:34:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+var count = 0;
+function updateValues(){
+ document.getElementById("selfValue").innerHTML=document.getElementById("selfSlider").value;
+ document.getElementById("societyValue").innerHTML=document.getElementById("societySlider").value;
+}
+window.onload=function(){
+ updateValues();
+ document.getElementById("selfSlider").oninput=updateValues;
+ document.getElementById("societySlider").oninput=updateValues;
+ document.getElementById("addButton").onclick=function(){
+  count=count+1;
+  document.getElementById("answerCount").innerHTML=count+" Antworten";
+ };
+ document.getElementById("clearButton").onclick=function(){
+  count=0;
+  document.getElementById("answerCount").innerHTML='0 Antworten';
+ };
+};
 
